e67d65e74084f972206ae791c7b7801a3d22f7b6
Ernst Widerberg <ernstwi@kth.se>
2023-11-04 14:24:35 +0100

    Fix

--- client.js
+++ client.js
@@ -9,9 +9,9 @@ function nextEp() {
     })
     .then((eps) => {
       let ep = eps[getRandomInt(eps.length)];
-      document.getElementById("title").src = ep.title;
-      document.getElementById("number").src = ep.number;
-      document.getElementById("date").src = ep.date;
+      document.getElementById("title").innerText = ep.title;
+      document.getElementById("number").innerText = ep.number;
+      document.getElementById("date").innerText = ep.date;
       document.getElementById("mp3").src = ep.mp3;
     });
 }
